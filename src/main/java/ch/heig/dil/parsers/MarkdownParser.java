f2843a9012662fff7a4a7d176364e3dcfe474427
Jonathan <chil.tfm@gmail.com>
2022-03-31 11:43:27 +0200

    refactor: reformat codes to the stantards of this project

--- src/main/java/ch/heig/dil/parsers/MarkdownParser.java
+++ src/main/java/ch/heig/dil/parsers/MarkdownParser.java
@@ -5,17 +5,17 @@ import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
 
 public class MarkdownParser {
-    public static String markdownToHtml(String line){
+    public static String markdownToHtml(String line) {
         Parser parser = Parser.builder().build();
         Node document = parser.parse(line);
         HtmlRenderer renderer = HtmlRenderer.builder().build();
-        return renderer.render(document);  // "<p>This is <em>Sparta</em></p>\n"
+        return renderer.render(document); // "<p>This is <em>Sparta</em></p>\n"
     }
 
-    public static String markdownToHtmlImage(String line){
+    public static String markdownToHtmlImage(String line) {
         String temp = markdownToHtml(line);
         temp = temp.substring(3);
-        temp = temp.substring(0, temp.length()-5);
+        temp = temp.substring(0, temp.length() - 5);
         StringBuilder sb = new StringBuilder(temp);
         sb.delete(temp.length() - 3, temp.length() - 2).append("\n");
         return sb.toString();
--- src/test/java/ch/heig/dil/parsers/MarkdownParserTest.java
+++ src/test/java/ch/heig/dil/parsers/MarkdownParserTest.java
@@ -1,34 +1,36 @@
 package ch.heig.dil.parsers;
 
-import ch.heig.dil.Boomshot;
-import org.junit.jupiter.api.Test;
-import picocli.CommandLine;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.commonmark.node.*;
-import org.commonmark.parser.Parser;
-import org.commonmark.renderer.html.HtmlRenderer;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class MarkdownParserTest {
 
     @Test
     void paragraph() {
-        assertEquals("<p>This is <em>Sparta</em></p>\n", MarkdownParser.markdownToHtml("This is *Sparta*"));
+        assertEquals(
+                "<p>This is <em>Sparta</em></p>\n",
+                MarkdownParser.markdownToHtml("This is *Sparta*"));
     }
 
     @Test
-    void title1(){
-        assertEquals("<h1>Mon premier article</h1>\n", MarkdownParser.markdownToHtml("# Mon premier article"));
+    void title1() {
+        assertEquals(
+                "<h1>Mon premier article</h1>\n",
+                MarkdownParser.markdownToHtml("# Mon premier article"));
     }
 
     @Test
-    void title2(){
-        assertEquals("<h2>Mon sous-titre</h2>\n", MarkdownParser.markdownToHtml("## Mon sous-titre"));
+    void title2() {
+        assertEquals(
+                "<h2>Mon sous-titre</h2>\n", MarkdownParser.markdownToHtml("## Mon sous-titre"));
     }
 
     @Test
-    void image(){
-        assertEquals("<img src=\"./image.png\" alt=\"Une image\"/>\n", MarkdownParser.markdownToHtmlImage("![Une image](./image.png)"));
+    void image() {
+        assertEquals(
+                "<img src=\"./image.png\" alt=\"Une image\"/>\n",
+                MarkdownParser.markdownToHtmlImage("![Une image](./image.png)"));
     }
 }
