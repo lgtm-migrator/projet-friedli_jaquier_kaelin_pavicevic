a05eb99d7365606e2a297e673dc485380f9f48a2
AJaquier <alex.jaquier@gmail.com>
2022-03-31 12:22:16 +0200

    feat: Parse YAML from a string and tests throws

--- src/main/java/ch/heig/dil/parsers/ParserYAML.java
+++ src/main/java/ch/heig/dil/parsers/ParserYAML.java
@@ -2,22 +2,31 @@ package ch.heig.dil.parsers;
 
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.fasterxml.jackson.dataformat.yaml.YAMLParser;
-
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Classe permettant de parser le contenu d'un fichier YAML et récupérer les métadonnées
+ *
+ * @author Alexandre Jaquier
+ */
 public class ParserYAML {
-    public static Map<String,String> Parse(String yamlString){
-        Map<String,String> meta = new HashMap<>();
+
+    /**
+     * Méthode statique permettant de parser une chaîne de caractères, provenant d'un fichier YAML
+     *
+     * @param yamlString contenu du fichier
+     * @return Métadonnées du fichier YAML
+     */
+    public static Map<String, String> getMetaDataFromString(String yamlString) throws IOException {
+        Map<String, String> meta = new HashMap<>();
         YAMLFactory factory = new YAMLFactory();
-        try(YAMLParser parser = factory.createParser(yamlString);){
-            while (parser.nextToken() != null) {
-                meta.put(parser.getCurrentName(), parser.getText());
-            }
-            meta.remove(null);
-        } catch (IOException e){
+        YAMLParser parser = factory.createParser(yamlString);
+        while (parser.nextToken() != null) {
+            meta.put(parser.getCurrentName(), parser.getText());
         }
+        meta.remove(null);
         System.out.println(meta);
         return meta;
     }
--- src/test/java/ch/heig/dil/parsers/ParserYAMLTest.java
+++ src/test/java/ch/heig/dil/parsers/ParserYAMLTest.java
@@ -1,23 +1,31 @@
 package ch.heig.dil.parsers;
 
-import org.junit.jupiter.api.Test;
-import java.util.HashMap;
-import java.util.Map;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
 public class ParserYAMLTest {
-    
+
     @Test
     void canParseYAML() {
-        Map<String,String> mapToCompare = new HashMap<>();
-        mapToCompare.put("titre","Title");
-        mapToCompare.put("auteur","Author");
-        mapToCompare.put("date","2022-03-19");
-        
-        String toParse = "titre: Title\n" +
-                "auteur: Author\n" +
-                "date: 2022-03-19";
-        Map<String,String> result = ParserYAML.Parse(toParse);
-        assertEquals(mapToCompare,result);
+        Map<String, String> mapToCompare = new HashMap<>();
+        mapToCompare.put("titre", "Title");
+        mapToCompare.put("auteur", "Author");
+        mapToCompare.put("date", "2022-03-19");
+
+        String toParse = "titre: Title\n" + "auteur: Author\n" + "date: 2022-03-19";
+
+        Map<String, String> result =
+                assertDoesNotThrow(() -> ParserYAML.getMetaDataFromString(toParse));
+        assertEquals(mapToCompare, result);
+    }
+
+    @Test
+    void ParseYAMLThrowsIOException() {
+        String toParse = "titre: Title\n" + "Author\n" + "date: 2022-03-19";
+        assertThrows(IOException.class, () -> ParserYAML.getMetaDataFromString(toParse));
     }
 }
