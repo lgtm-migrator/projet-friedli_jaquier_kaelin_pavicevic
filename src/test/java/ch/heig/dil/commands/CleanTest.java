5fee5329bbc32c69d7e2a5e7537339dd7102129f
AJaquier <alex.jaquier@gmail.com>
2022-04-07 12:10:35 +0200

    feat: tests for the clean command

--- src/test/java/ch/heig/dil/commands/CleanTest.java
+++ src/test/java/ch/heig/dil/commands/CleanTest.java
@@ -1,38 +1,39 @@
 package ch.heig.dil.commands;
 
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.io.*;
+import java.nio.file.Files;
 
 import ch.heig.dil.Boomshot;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+import com.sun.tools.javac.Main;
 import org.junit.jupiter.api.Test;
 import picocli.CommandLine;
 
 class CleanTest {
-
+    
     @Test
-    void result() {
-        int exitCode = new CommandLine(new Boomshot()).execute("clean");
-        assertEquals(exitCode, 0);
-    }
-
-    @Test
-    void exception() {
-        assertThrows(
-                Exception.class,
-                () -> {
-                    throw new Exception();
-                });
-    }
-
-    @Test
-    void output() throws Exception {
-        try (ByteArrayOutputStream output = new ByteArrayOutputStream()) {
-            System.setOut(new PrintStream(output));
-            new CommandLine(new Boomshot()).execute("clean");
-            assertTrue((output.toString().contains("Clean command")));
-        }
+    void testClean() {
+        String dirName = "CleanTest/build";
+        assertFalse(Files.exists(new File("CleanTest").toPath()));
+        new File(dirName).mkdirs();
+        
+        //Delete the previously created build folder by using the clean command
+        Boomshot app = new Boomshot();
+        StringWriter sw = new StringWriter();
+        CommandLine cmd = new CommandLine(app);
+        cmd.setOut(new PrintWriter(sw));
+        
+        File path = new File("CleanTest/build");
+        
+        int exitCode = cmd.execute("clean", "/CleanTest");
+        boolean fileExist = Files.exists(path.toPath());
+        
+        File site = new File("CleanTest");
+        site.delete();
+        assertEquals(0, exitCode);
+        
     }
 }
