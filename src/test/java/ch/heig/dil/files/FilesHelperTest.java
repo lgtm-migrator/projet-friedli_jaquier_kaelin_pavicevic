5ab89daafde61162e9bc10c214a2a15a2b6d8b0d
Jonathan <chil.tfm@gmail.com>
2022-04-07 12:45:18 +0200

    feat: create test for file reader

---
+++ src/test/java/ch/heig/dil/files/FilesHelperTest.java
@@ -0,0 +1,27 @@
+package ch.heig.dil.files;
+
+import ch.heig.dil.parsers.MarkdownParser;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FilesHelperTest {
+
+    @Test
+    void fileDoesNotExist() {
+        assertThrows(
+                Exception.class,
+                () -> {
+                    FilesHelper.readFromFile("");
+                });
+    }
+
+    //
+    @Test
+    void readFromAbsoluteFile(){
+        assertEquals(
+                "Je suis un texte\n",
+                FilesHelper.readFromFile("C:\\Users\\Jonathan\\HEIG\\SEMESTRE_4\\DIL\\Labo2\\projet-friedli_jaquier_kaelin_pavicevic\\src\\test\\java\\ch\\heig\\dil\\files\\alo.txt"));
+    }
+}
