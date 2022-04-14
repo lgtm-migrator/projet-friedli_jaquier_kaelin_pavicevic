8eb0378d810945db868fc4379a3f4f6e6f5abf65
Valentin Kaelin <vkaelin@hey.com>
2022-04-14 14:51:22 +0200

    refactor: put files in resources folder, not working in CLI for now

--- src/main/java/ch/heig/dil/commands/Init.java
+++ src/main/java/ch/heig/dil/commands/Init.java
@@ -1,5 +1,6 @@
 package ch.heig.dil.commands;
 
+import ch.heig.dil.Boomshot;
 import ch.heig.dil.files.FilesHelper;
 import java.io.IOException;
 import java.util.concurrent.Callable;
@@ -7,14 +8,17 @@ import picocli.CommandLine;
 
 @CommandLine.Command(name = "init", description = "Init a new static website.")
 public class Init implements Callable<Integer> {
-    private static final String TEMPLATES_PATH = "templates/init";
+    private static final String TEMPLATES_PATH = "/templates/init";
 
     @CommandLine.Parameters(description = "Path of the static website")
     String path;
 
     @Override
     public Integer call() throws IOException {
-        FilesHelper.copyDirectory(TEMPLATES_PATH, path);
+        var templateRessource = Boomshot.class.getResource(TEMPLATES_PATH);
+        if (templateRessource == null) return 1;
+
+        FilesHelper.copyDirectory(templateRessource.getPath(), path);
         System.out.println("Website initialized.");
         return 0;
     }
