9a31b2a9bb604511b93e39353061718077441872
Jonathan <chil.tfm@gmail.com>
2022-04-14 12:03:39 +0200

    refactor: apply spotless

--- src/main/java/ch/heig/dil/parsers/PageParser.java
+++ src/main/java/ch/heig/dil/parsers/PageParser.java
@@ -1,15 +1,12 @@
 package ch.heig.dil.parsers;
 
 import java.io.IOException;
-import java.util.Map;
 
 public class PageParser {
     public static String parse(String file) throws IOException {
         String[] splittedString = file.split("---");
-        if(splittedString.length != 2)
+        if (splittedString.length != 2)
             throw new IOException("The splitting string is not in the file");
-        //Map<String,String> tokens = ParserYAML.getMetaDataFromString(splittedString[0]);
-        String html = MarkdownParser.markdownToHtml(splittedString[1]);
-        return html;
+        return MarkdownParser.markdownToHtml(splittedString[1]);
     }
 }
