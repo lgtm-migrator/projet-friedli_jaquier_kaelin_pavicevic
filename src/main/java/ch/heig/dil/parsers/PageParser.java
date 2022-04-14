aa3500e7b9096d4e4f835b1b3a3fd8aef6a91f27
AJaquier <alex.jaquier@gmail.com>
2022-04-14 11:51:06 +0200

    feat: Page Parser Tests

--- src/main/java/ch/heig/dil/parsers/PageParser.java
+++ src/main/java/ch/heig/dil/parsers/PageParser.java
@@ -6,6 +6,8 @@ import java.util.Map;
 public class PageParser {
     public static String parse(String file) throws IOException {
         String[] splittedString = file.split("---");
+        if(splittedString.length != 2)
+            throw new IOException("The splitting string is not in the file");
         //Map<String,String> tokens = ParserYAML.getMetaDataFromString(splittedString[0]);
         String html = MarkdownParser.markdownToHtml(splittedString[1]);
         return html;
