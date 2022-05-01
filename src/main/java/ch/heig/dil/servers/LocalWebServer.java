66a35bd8a7f7dc5b3360200b6295cd37d2d9a3e3
Lazzzer <lazazr@gmail.com>
2022-05-01 20:00:00 +0200

    refactor: add more test and add constructor with directory

--- src/main/java/ch/heig/dil/servers/LocalWebServer.java
+++ src/main/java/ch/heig/dil/servers/LocalWebServer.java
@@ -1,9 +1,10 @@
 package ch.heig.dil.servers;
 
 import io.javalin.Javalin;
+import io.javalin.core.util.JavalinException;
 import io.javalin.core.util.JavalinLogger;
 import io.javalin.http.staticfiles.Location;
-import java.io.IOException;
+import org.eclipse.jetty.util.log.Log;
 
 /**
  * Serveur web local permettant d'afficher le contenu du site statique
@@ -13,38 +14,46 @@ import java.io.IOException;
 public class LocalWebServer {
     private final Javalin server;
 
-    /**
-     * Constructeur par défaut bindant le serveur au port 8080.
-     *
-     * @throws IOException
-     */
-    public LocalWebServer() throws IOException {
+    /** Constructeur par défaut utilisant le port 8080 et le répertoir "public". */
+    public LocalWebServer() {
         this(8080);
     }
 
     /**
-     * Constructeur permettant de choisir le port à bind.
+     * Constructeur permettant seuleument de choisir le port à bind et utilise le répertoir "public.
      *
      * @param port Port à utiliser
-     * @throws RuntimeException levée quand le port est invalide
      */
     public LocalWebServer(int port) {
-        if (port < 1) throw new RuntimeException("Error : invalid port");
+        this(port, "public");
+    }
+
+    /**
+     * Constructeur permettant de choisir le port à bind et le chemin du répertoire contenant les
+     * fichiers.
+     *
+     * @param port Port à utiliser
+     * @param directory Dossier contenant les pages statiques à afficher
+     * @throws IllegalArgumentException levée quand le port est invalide
+     * @throws JavalinException levée quand le directory est invalide
+     */
+    public LocalWebServer(int port, String directory)
+            throws IllegalArgumentException, JavalinException {
+        if (port < 1) throw new IllegalArgumentException("Error : invalid port");
         System.out.println("Listening on port " + port);
         server =
                 Javalin.create(
                                 config -> {
-                                    org.eclipse.jetty.util.log.Log.getProperties()
+                                    Log.getProperties()
                                             .setProperty("org.eclipse.jetty.LEVEL", "OFF");
-                                    org.eclipse.jetty.util.log.Log.getProperties()
+                                    Log.getProperties()
                                             .setProperty(
                                                     "org.eclipse.jetty.util.log.announce", "false");
                                     JavalinLogger.startupInfo = false;
                                     JavalinLogger.enabled = false;
                                     config.addStaticFiles(
                                             staticFileConfig -> {
-                                                staticFileConfig.hostedPath = "/";
-                                                staticFileConfig.directory = "testing"; // A changer
+                                                staticFileConfig.directory = directory;
                                                 staticFileConfig.location = Location.EXTERNAL;
                                             });
                                 })
--- src/test/java/ch/heig/dil/servers/LocalWebServerTest.java
+++ src/test/java/ch/heig/dil/servers/LocalWebServerTest.java
@@ -2,6 +2,7 @@ package ch.heig.dil.servers;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import io.javalin.core.util.JavalinException;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
@@ -15,37 +16,32 @@ public class LocalWebServerTest {
 
     @Test
     void ConstructorDoesntThrow() {
-        var server = assertDoesNotThrow(() -> new LocalWebServer(4242));
+        var server = assertDoesNotThrow(() -> new LocalWebServer());
         server.stop();
     }
 
     @Test
-    void ConstructorThrows() {
-        assertThrows(RuntimeException.class, () -> new LocalWebServer(-1000));
+    void ConstructorThrowsIllegalArgument() {
+        assertThrows(IllegalArgumentException.class, () -> new LocalWebServer(-1000, "public"));
+    }
+
+    @Test
+    void ConstructorThrowsJavalinException() {
+        assertThrows(JavalinException.class, () -> new LocalWebServer(5000, "fakeDirectory"));
     }
 
     @Test
     void WebServerIsListening() throws IOException {
         try (ByteArrayOutputStream output = new ByteArrayOutputStream()) {
             System.setOut(new PrintStream(output));
-            var server = assertDoesNotThrow(() -> new LocalWebServer(4242));
-            assertTrue((output.toString().contains("Listening on port 4242")));
-            server.stop();
-        }
-    }
-
-    @Test
-    void WebServerIsListeningOnDefaultPort() throws IOException {
-        try (ByteArrayOutputStream output = new ByteArrayOutputStream()) {
-            System.setOut(new PrintStream(output));
             var server = assertDoesNotThrow(() -> new LocalWebServer());
-            server.stop();
             assertTrue((output.toString().contains("Listening on port 8080")));
+            server.stop();
         }
     }
 
     @Test
-    void WebServerGivesStatusCode200ForIndex() {
+    void ServerGivesStatusCode200ForIndex() {
 
         var server = assertDoesNotThrow(() -> new LocalWebServer());
         int port = 8080;
@@ -64,5 +60,47 @@ public class LocalWebServerTest {
         System.out.println(response.body());
         server.stop();
         assertEquals(response.statusCode(), 200);
+    }
+
+    @Test
+    void ServerGivesStatusCode200ForNestedPage() {
+
+        var server = assertDoesNotThrow(() -> new LocalWebServer());
+        int port = 8080;
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request =
+                HttpRequest.newBuilder()
+                        .uri(URI.create("http://localhost:" + port + "/nested/page.html"))
+                        .header("Content-Type", "text/html")
+                        .GET()
+                        .build();
+
+        HttpResponse<String> response =
+                assertDoesNotThrow(
+                        () -> client.send(request, HttpResponse.BodyHandlers.ofString()));
+
+        System.out.println(response.body());
+        server.stop();
+        assertEquals(response.statusCode(), 200);
+    }
+
+    @Test
+    void ServerGivesStatusCode404() {
+
+        var server = assertDoesNotThrow(() -> new LocalWebServer());
+        int port = 8080;
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request =
+                HttpRequest.newBuilder()
+                        .uri(URI.create("http://localhost:" + port + "/fakePage"))
+                        .header("Content-Type", "text/html")
+                        .GET()
+                        .build();
+
+        HttpResponse<String> response =
+                assertDoesNotThrow(
+                        () -> client.send(request, HttpResponse.BodyHandlers.ofString()));
+        server.stop();
+        assertEquals(response.statusCode(), 404);
     }
 }
