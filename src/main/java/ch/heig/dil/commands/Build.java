3f6aff4f7cbf899625e8a94eba1a9ce57e02f1b1
Valentin Kaelin <vkaelin@hey.com>
2022-05-04 22:56:54 +0200

    feat: add the first version of the build command

--- src/main/java/ch/heig/dil/commands/Build.java
+++ src/main/java/ch/heig/dil/commands/Build.java
@@ -1,15 +1,59 @@
 package ch.heig.dil.commands;
 
+import ch.heig.dil.files.FilesHelper;
+import ch.heig.dil.parsers.PageParser;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.concurrent.Callable;
 import picocli.CommandLine;
 
+/**
+ * Build le site statique en transformant les fichiers markdown en html
+ *
+ * @author Valentin Kaelin
+ */
 @CommandLine.Command(name = "build", description = "Build the static website.")
 public class Build implements Callable<Integer> {
-
+    
+    @CommandLine.Parameters(index = "0", description = "Source of the static website")
+    private String sourcePath;
+    
+    public static final List<String> excluded = List.of("config.yml", "build");
+    
     @Override
     public Integer call() throws IOException {
-        System.out.println("Build command");
+        String BUILD_DIRECTORY = "build";
+        FilesHelper.cleanDirectoy(BUILD_DIRECTORY);
+        Path out = Paths.get(BUILD_DIRECTORY);
+        Path sourceOfPath = Paths.get(sourcePath);
+        
+        Files.walk(sourceOfPath)
+                .filter(Files::isRegularFile)
+                .filter(p -> !excluded.contains(p.getFileName().toString()))
+                .forEach(
+                        source -> {
+                            Path destination = out.resolve(sourceOfPath.relativize(source));
+                            try {
+                                Files.createDirectories(destination.getParent());
+                                if (source.getFileName().toString().endsWith(".md")) {
+                                    destination =
+                                            Path.of(destination.toString().replace(".md", ".html"));
+                                    String markdown = PageParser.parse(Files.readString(source));
+                                    
+                                    Files.writeString(destination, markdown);
+                                } else {
+                                    Files.copy(source, destination);
+                                }
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        });
+        
+        System.out.println("Static website built.");
+        
         return 0;
     }
 }
