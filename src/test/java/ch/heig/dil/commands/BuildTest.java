700500e4d517a2ca8cf748607ab6b4d17b6c1fbd
Valentin Kaelin <vkaelin@hey.com>
2022-05-04 22:56:40 +0200

    test: add some tests for the future build command

--- src/test/java/ch/heig/dil/commands/BuildTest.java
+++ src/test/java/ch/heig/dil/commands/BuildTest.java
@@ -1,38 +1,70 @@
 package ch.heig.dil.commands;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-import ch.heig.dil.Boomshot;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+import ch.heig.dil.files.FilesHelper;
+import java.io.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import picocli.CommandLine;
 
 class BuildTest {
+    private static final String INIT_FOLDER = "./test-build";
+    private static final String BUILD_FOLDER = "build";
 
-    @Test
-    void result() {
-        int exitCode = new CommandLine(new Boomshot()).execute("build");
-        assertEquals(exitCode, 0);
-    }
-
-    @Test
-    void exception() {
-        assertThrows(
-                Exception.class,
-                () -> {
-                    throw new Exception();
-                });
-    }
-
-    @Test
-    void output() throws Exception {
-        try (ByteArrayOutputStream output = new ByteArrayOutputStream()) {
-            System.setOut(new PrintStream(output));
-            new CommandLine(new Boomshot()).execute("build");
-            assertTrue((output.toString().contains("Build command")));
+    /** Crée le dossier de test avant chaque test */
+    @BeforeEach
+    protected void setup() {
+        try {
+            FilesHelper.deleteDirectory(BUILD_FOLDER);
+            FilesHelper.deleteDirectory(INIT_FOLDER);
+        } catch (Exception ignored) {
         }
+        String[] args = new String[] {INIT_FOLDER};
+        CommandLine cmd = new CommandLine(new Init());
+        cmd.execute(args);
+    }
+
+    /**
+     * Supprime le dossier de test après chaque test
+     *
+     * @throws IOException si une erreur apparait lors de la suppression du dossier
+     */
+    @AfterEach
+    protected void clean() throws IOException {
+        FilesHelper.deleteDirectory(INIT_FOLDER);
+        FilesHelper.deleteDirectory(BUILD_FOLDER);
+    }
+
+    @Test
+    void testNormalBuildBehaviour() {
+        String[] args = new String[] {INIT_FOLDER};
+        CommandLine cmd = new CommandLine(new Build());
+        cmd.execute(args);
+
+        assertTrue(new File(BUILD_FOLDER + "/index.html").exists());
+        assertTrue(new File(BUILD_FOLDER + "/pages/image.jpeg").exists());
+        assertTrue(new File(BUILD_FOLDER + "/pages/page.html").exists());
+    }
+
+    @Test
+    void testBuildIfAlreadyExist() {
+        String[] args = new String[] {INIT_FOLDER};
+        CommandLine cmd = new CommandLine(new Build());
+        cmd.execute(args);
+        cmd.execute(args);
+
+        assertTrue(new File(BUILD_FOLDER + "/index.html").exists());
+        assertTrue(new File(BUILD_FOLDER + "/pages/image.jpeg").exists());
+        assertTrue(new File(BUILD_FOLDER + "/pages/page.html").exists());
+    }
+
+    @Test
+    void testBuildOnNotInitializedWebsite() {
+        String[] args = new String[] {"invalid-path-to-static-website"};
+        CommandLine cmd = new CommandLine(new Build());
+        cmd.execute(args);
+        assertThrows(IOException.class, () -> cmd.execute(args));
     }
 }
