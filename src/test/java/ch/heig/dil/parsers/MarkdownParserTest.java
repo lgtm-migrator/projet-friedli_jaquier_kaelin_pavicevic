df11715c8e3974a44efe63fa12711de79d100fce
Valentin Kaelin <vkaelin@hey.com>
2022-05-05 13:42:50 +0200

    refactor: transform to handlerbars helper the markdown parser

--- src/test/java/ch/heig/dil/parsers/MarkdownParserTest.java
+++ src/test/java/ch/heig/dil/parsers/MarkdownParserTest.java
@@ -7,40 +7,30 @@ import org.junit.jupiter.api.Test;
 
 public class MarkdownParserTest {
 
+    private final MarkdownParser parser = new MarkdownParser();
+
     @Test
     void translateSimpleParagraph() {
-        assertEquals(
-                "<p>This is <em>Sparta</em></p>\n",
-                MarkdownParser.markdownToHtml("This is *Sparta*"));
+        assertEquals("<p>This is <em>Sparta</em></p>\n", parser.apply(("This is *Sparta*"), null));
     }
 
     @Test
     void translateFailedBoldText() {
-        assertEquals("<p>I am *bold</p>\n", MarkdownParser.markdownToHtml("I am *bold"));
+        assertEquals("<p>I am *bold</p>\n", parser.apply("I am *bold", null));
     }
 
     @Test
     void translateFailedBacktickText() {
-        assertEquals("<p>I am ```code``</p>\n", MarkdownParser.markdownToHtml("I am ```code``"));
+        assertEquals("<p>I am ```code``</p>\n", parser.apply("I am ```code``", null));
     }
 
     @Test
     void translateTitle1() {
-        assertEquals(
-                "<h1>Mon premier article</h1>\n",
-                MarkdownParser.markdownToHtml("# Mon premier article"));
+        assertEquals("<h1>Mon premier article</h1>\n", parser.apply("# Mon premier article", null));
     }
 
     @Test
     void translateTitle2() {
-        assertEquals(
-                "<h2>Mon sous-titre</h2>\n", MarkdownParser.markdownToHtml("## Mon sous-titre"));
-    }
-
-    @Test
-    void translateImage() {
-        assertEquals(
-                "<img src=\"./image.png\" alt=\"Une image\"/>\n",
-                MarkdownParser.markdownToHtmlImage("![Une image](./image.png)"));
+        assertEquals("<h2>Mon sous-titre</h2>\n", parser.apply("## Mon sous-titre", null));
     }
 }
