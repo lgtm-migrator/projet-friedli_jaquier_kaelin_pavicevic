14bec30286e461f618635832730cc32a79ea670f
Jonathan <chil.tfm@gmail.com>
2022-05-05 12:17:46 +0200

    feat: the display version now works fine

--- src/main/java/ch/heig/dil/Boomshot.java
+++ src/main/java/ch/heig/dil/Boomshot.java
@@ -1,13 +1,15 @@
 package ch.heig.dil;
 
 import ch.heig.dil.commands.*;
+
+import java.net.URL;
+import java.util.Properties;
 import java.util.concurrent.Callable;
 import picocli.CommandLine;
 
 @CommandLine.Command(
         mixinStandardHelpOptions = true,
-        name = "boomshot",
-        version = "0.1",
+        versionProvider = Boomshot.VersionProvide.class,
         description = "A static site generator",
         subcommands = {New.class, Clean.class, Build.class, Serve.class})
 public class Boomshot implements Callable<Integer> {
@@ -20,5 +22,20 @@ public class Boomshot implements Callable<Integer> {
     public Integer call() throws Exception {
         CommandLine.usage(this, System.out);
         return 0;
+    }
+
+    static class VersionProvide implements CommandLine.IVersionProvider {
+
+        public String[] getVersion() throws Exception {
+            URL url = getClass().getResource("/version.properties");
+            if (url == null) {
+                return new String[]{"No version.properties file found in the classpath. Is examples.jar in the classpath?"};
+            }
+            Properties properties = new Properties();
+            properties.load(url.openStream());
+            return new String[]{
+                    properties.getProperty("application") + " " + properties.getProperty("version")
+            };
+        }
     }
 }
