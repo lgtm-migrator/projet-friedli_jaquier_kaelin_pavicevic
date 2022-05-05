2a0a75d2ec99cc00a21100ae7a8aba8653ee58d6
Jonathan <chil.tfm@gmail.com>
2022-05-05 12:29:34 +0200

    docs: add comments

--- src/main/java/ch/heig/dil/Boomshot.java
+++ src/main/java/ch/heig/dil/Boomshot.java
@@ -6,6 +6,11 @@ import java.util.Properties;
 import java.util.concurrent.Callable;
 import picocli.CommandLine;
 
+/**
+ * Classe principale du projet Boomshot
+ *
+ * @author Jonathan Friedli
+ */
 @CommandLine.Command(
         mixinStandardHelpOptions = true,
         versionProvider = Boomshot.VersionProvide.class,
@@ -23,14 +28,20 @@ public class Boomshot implements Callable<Integer> {
         return 0;
     }
 
+    /** Classe interne permettant de récuper la version du projet dans le pom.xml */
     static class VersionProvide implements CommandLine.IVersionProvider {
 
+        /**
+         * Renvoie la version du projet qui se trouve dans le fichier pom.xml. Nous avons un fichier
+         * version.properties qui fait le lien entre le pom.xml et le versionProvide
+         *
+         * @return La version
+         * @throws Exception Si on ne trouve pas le fichier version.properties
+         */
         public String[] getVersion() throws Exception {
             URL url = getClass().getResource("/version.properties");
             if (url == null) {
-                return new String[] {
-                    "No version.properties file found in the classpath. Is examples.jar in the classpath?"
-                };
+                return new String[] {"No version.properties file found in the classpath !"};
             }
             Properties properties = new Properties();
             properties.load(url.openStream());
