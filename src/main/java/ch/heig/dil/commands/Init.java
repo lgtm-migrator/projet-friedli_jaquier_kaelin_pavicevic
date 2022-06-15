9ee69f3851e2ba819c572cc5c284b7707b1eb8e7
Kalane <vkaelin@hey.com>
2022-06-15 16:35:55 +0200

    feat: first attempt to fix init command in cli

--- src/main/java/ch/heig/dil/commands/Init.java
+++ src/main/java/ch/heig/dil/commands/Init.java
@@ -1,7 +1,12 @@
 package ch.heig.dil.commands;
 
 import ch.heig.dil.files.FilesHelper;
+import java.net.URI;
+import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Callable;
 import picocli.CommandLine;
 
@@ -15,13 +20,24 @@ public class Init implements Callable<Integer> {
     private static final String TEMPLATES_PATH = "/templates/init";
 
     @CommandLine.Parameters(description = "Path of the static website")
-    String path;
+    Path path;
 
     @Override
     public Integer call() {
         try {
-            Path templateRessource = FilesHelper.getRessourcePath(TEMPLATES_PATH);
-            FilesHelper.copyDirectory(templateRessource.toString(), path);
+            // Path templateRessource = FilesHelper.getRessourcePath(TEMPLATES_PATH);
+
+            URI uri = this.getClass().getResource(TEMPLATES_PATH).toURI();
+
+            // Initialize a zip file system when the template is stored in a jar file
+            if (uri.getScheme().equals("jar")) {
+                Map<String, String> env = new HashMap<>();
+                env.put("create", "true");
+                FileSystems.newFileSystem(uri, env);
+            }
+
+            Path template = Paths.get(uri);
+            FilesHelper.copyDirectory(template, path);
             System.out.println("Website initialized.");
         } catch (Exception ex) {
             return 1;
